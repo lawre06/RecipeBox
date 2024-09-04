ab5b74c94731776eecfd49cf6f86040bf16fe6e3
Joel Lawrence <123508378+lawre06@users.noreply.github.com>
2024-09-04 03:03:27 -0400

    Update index.js
    Made separate file for routing, will create separate app and router for clarity

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { config } from "dotenv";
+import mongoose from 'mongoose';
+import { Recipe } from './model';
 import cors from 'cors';
-import { MongoClient } from "mongodb";
+
 const app = express();
 const port = 4000;
 
@@ -10,37 +11,20 @@ app.use(cors());
 app.use(express.json());
 
 
-const url = "mongodb+srv://pacific9682:Rwlabf6NOx00Vn4H@recipecluster.dveki4c.mongodb.net/?retryWrites=true&w=majority&appName=RecipeCluster"
-
-const client = new MongoClient(url)
-async function run() {
-    try {
-        await client.connect();
-        console.log("Successfully connected to Atlas");
-    } catch (err) {
-        console.log(err.stack);
-    }
-    finally {
-        await client.close();
-    }
-}
-
-
 app.post('/recipes', async(req, res) => {
     console.log(req)
     const newRecipe = {
         title : req.title,
         content : req.content
     }
-
+    return res.status().json(newRecipe);
 });
 
 app.get('/recipes/:id', async(req, res) => {
     console.log(req)
+    
 });
 
 app.delete('/recipes/delete', async(req, res) => {
     console.log(req)
 });
-
-run();
