e9c5590686eb4b62b76e9dd9c5785e9157f76b4b
Joel Lawrence <123508378+lawre06@users.noreply.github.com>
2024-09-06 01:51:41 -0400

    Update routing.js
    Finished routes for each feature, separated app and router for modularity and clarity.

--- backend/routing.js
+++ backend/routing.js
@@ -1,30 +1,58 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { Recipe } from './model';
+import { Recipe } from './model.js';
 import cors from 'cors';
 
-const app = express();
-const port = 4000;
+const router = express.Router();
 
 
-app.use(cors());
-app.use(express.json());
-
-
-app.post('/recipes', async(req, res) => {
-    console.log(req)
+router.post('/recipes', async(req, res) => {
+    console.log(req);
     const newRecipe = {
         title : req.title,
-        content : req.content
+        description : req.description
     }
-    return res.status().json(newRecipe);
+    const recipe = await Recipe.create(newRecipe);
+    return res.status(201).json(recipe);
+
 });
 
-app.get('/recipes/:id', async(req, res) => {
-    console.log(req)
+router.get('/recipes/all', async(req, res) => {
+    console.log(req);
+    const recipes = await Recipe.find({});
+
+    return res.status(200).json({
+        count: recipes.length,
+        data: recipes
+    })
+});
+
+router.delete('/recipes/:id', async(req, res) => {
+    console.log(req);
+    id = req.params['id'];
+    value = await Recipe.findByIdAndDelete(id);
     
+    return res.status().json({message: 'Recipe Deleted'});
+
 });
 
-app.delete('/recipes/delete', async(req, res) => {
-    console.log(req)
-});
+router.get('/recipes/:id', async(req, res) => {
+    console.log(req);
+    id = req.params['id'];
+    value = await Recipe.find(id);
+
+    return res.status().json(value);
+})
+
+router.put('/recipes/:id', async(req, res) => {
+    console.log(req);
+    id = req.params['id'];
+    editRecipe = {
+        title : req.title,
+        description : req.description
+    }
+    const value = Recipe.findByIDAndUpdate(id, editRecipe);
+    return res.status(201).json(value);
+})
+
+export default router;
