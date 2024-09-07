db2ab1ffc370792ec04a463aa69acbedf9c0d6ab
Joel Lawrence <123508378+lawre06@users.noreply.github.com>
2024-09-07 02:24:41 -0400

    Update model.js

--- backend/model.js
+++ backend/model.js
@@ -3,10 +3,12 @@ import mongoose from 'mongoose';
 const recipeSchema = mongoose.Schema(
     {
         name: {
-
+            type: String,
+            required: true
         },
         description: {
-
+            type: String,
+            required: true
         }
     }
 );
